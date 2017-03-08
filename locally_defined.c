0f825a56e438fe371988f296cb48b3db662e1153
Flühmann Tobias, INI-ON-NTO-COY-NNI <tobias.fluehmann@swisscom.com>
2017-03-08 01:28:11 +0100

    spread code into multiple functions

--- locally_defined.c
+++ locally_defined.c
@@ -3,13 +3,32 @@
 #include <math.h>
 #include <stdlib.h>
 #define max 5000
+#define waittime 10 
+
+void wait_for_seconds();
+int ** allocate(int **a);
+void dellocate(int **a);
 
 int main() {
   int **a;
+  a = allocate(a);
+  wait_for_seconds();
+  dellocate(a);
+  wait_for_seconds();
+  printf("huston over\n");
+}
+
+void dellocate(int **a){
+  for(int i = 0; i < max; i++)
+    free(a[i]);
+  free(a);
+  printf("freed memory\n");
+}
+
+int ** allocate(int **a){
   int i, j;
 
   a=(int **) malloc(max * sizeof(int *));
-
   for(i=0;i<max;i++)
     a[i]=(int *) malloc(max * sizeof(int));
 
@@ -18,32 +37,17 @@ int main() {
        a[i][j] = j;
   }
   printf("Last element %d\n\n", a[i-1][j-1]);
+  return a;
+}
 
-  int sec = 0, trigger = 10; // seconds
+void wait_for_seconds(){
+  int sec = 0; // seconds
   int counter = 0;
   clock_t before = clock();
-//  printf("clock %Lf", (long double) before);
-  printf("loop started \n");
-  while(sec < trigger){
+  printf("wait for %d seconds \n", waittime);
+  while(sec < waittime){
     clock_t diff = clock()-before;
-//    printf("clock %Lf", (long double) diff);
     counter++;
     sec = diff / 1000000;
-  }
-  printf("loop finished\n");
-/* 
-  long  long int_max = pow(2, 32);
-  long start;
-  printf("loop %lld\n", int_max);
-  for(start = 0; start < int_max; start++){
-  }
-  printf("loop end \n");
-
-  */
-  for(i =0; i< max; i++){
-    free(a[i]);
-  }
-  free(a);
-  while(1){
   }
 }
